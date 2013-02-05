354c71b1c830b8b0dd01af7a8f4fa594de01de0b
Cecilia Lindberg <contactcecilialindberg@gmail.com>
2013-02-05 17:45:46 +0100

    gjorde om till hello world

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,12 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Class1 anst1 = new Anstalld();
-            Anstalld anst2 = new Anstalld("Hans", "Dalby");
-            anst1.Namn = "Anna";
-            anst1.Adress = "Lund";
-            Console.WriteLine(anst1.Name + anst1.Adress);
-            Console.WriteLine(anst2.Name + anst2.Adress);
+            Console.WriteLine("Hello world");
         }
     }
 }
