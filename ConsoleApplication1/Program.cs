ccd0e3deadc91687c45711054b3b5d60a873b2f3
Cecilia Lindberg <contactcecilialindberg@gmail.com>
2013-02-05 17:39:41 +0100

    testa litttte

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class AnstalldTest
+    {
+        static void Main(string[] args)
+        {
+            Class1 anst1 = new Anstalld();
+            Anstalld anst2 = new Anstalld("Hans", "Dalby");
+            anst1.Namn = "Anna";
+            anst1.Adress = "Lund";
+            Console.WriteLine(anst1.Name + anst1.Adress);
+            Console.WriteLine(anst2.Name + anst2.Adress);
+        }
+    }
+}
